e374bed4946559a1d5eff5caf0d4935c943fc23e
Liviu Cernei <cernei.liviu@gmail.com>
2019-09-25 10:44:13 +0300

    Add haystack and calculator implementations

--- DailyKata/Katas/StringCalculatorKata.cs
+++ DailyKata/Katas/StringCalculatorKata.cs
@@ -6,7 +6,27 @@ namespace DailyKata
     {
         public static int Calculate(string input)
         {
-            throw new NotImplementedException();
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+            if (input == "")
+            {
+                throw new ArgumentException("Empty String", "input");
+            }
+
+            int result = 0;
+            string[] substrings = input.Split(',');
+            foreach (var item in substrings)
+            {
+                int number;
+                if (Int32.TryParse(item, out number))
+                {
+                    result += number;
+                }
+            }
+
+            return result;
         }
     }
 }
